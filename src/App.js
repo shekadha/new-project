9ebe61df67c682ed4c987e8ef7ff443cba9d38ba
shekadha <124559256+shekadha@users.noreply.github.com>
2023-05-29 07:08:34 +0330

    add links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <p>
+        This project is created by Shkoufeh Adhami and is
+        <a href="https://github.com/shekadha/new-project">
+          open-sourced on GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
